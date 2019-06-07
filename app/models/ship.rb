c5cc33a4c0169305f87357052e56fc6ef3829acb
Kishandth Sivapalasundaram <rodes88@msn.com>
2019-06-07 22:18:14 +0000

    Automatically backed up by Learn

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,3 +1,16 @@
 class Ship
+   attr_accessor :name, :type, :booty
   
+  @@all = []
+  
+  def initialize(name:, type:, booty:)
+    @name =  name 
+    @height = height
+    @booty = booty 
+  end 
+  
+  def self.all 
+    @@all
+  end 
+
 end
